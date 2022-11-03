a03fe3bf6b3ac8629a3258a8e31c7bf3a825bf70
youxin1231 <tommy135721@gmail.com>
2022-11-03 20:21:50 +0800

    path: Organize paths

--- HW2/src/preprocess.py
+++ HW2/src/preprocess.py
@@ -21,24 +21,46 @@ def parse_args():
 
 
 def preprocess(data, context):
-    preprocessed = []
+    swag = []; squad = []
+
     for i in range(len(data)):
+        # SWAG dataset format
         d = {}
+
         d['video-id'] = data[i]['id']
-        d['fold-ind'] = i
+        d['fold-ind'] = str(i)
         d['startphrase'] = data[i]['question']
+
         d['sent1'] = data[i]['question']
         d['sent2'] = ''
+
         d['gold-source'] = 'gold'
+
         d['ending0'] = context[data[i]['paragraphs'][0]]
         d['ending1'] = context[data[i]['paragraphs'][1]]
         d['ending2'] = context[data[i]['paragraphs'][2]]
         d['ending3'] = context[data[i]['paragraphs'][3]]
+
         for j in range(4):
             if(data[i]['relevant'] == data[i]['paragraphs'][j]):
                 d['label'] = j 
-        preprocessed.append(d)
-    return preprocessed
+        swag.append(d)
+
+        # SQuAD dataset format
+        d = {}
+
+        d['answers'] = {}
+        d['answers']['answer_start'] = [data[i]['answer']['start']]
+        d['answers']['text'] = [data[i]['answer']['text']]
+
+        d['context'] = context[data[i]['relevant']]
+        d['id'] = data[i]['id']
+
+        d['question'] = data[i]['question']
+        d['title'] = 'train'
+
+        squad.append(d)
+    return swag, squad
 
 
 def main():
@@ -52,12 +74,15 @@ def main():
     context = json.loads(context_path.read_text())
 
     for split in SPLITS:
-        preprocessed_file = preprocess(data[split], context)
+        swag, squad = preprocess(data[split], context)
         
-        output_path = args.output_dir / f"{split}.json"
-        with open(str(output_path), 'w', encoding='UTF-8') as f:
-            json.dump(preprocessed_file, f, indent=4, ensure_ascii=False)
+        swag_path = args.output_dir / f"{split}_swag.json"
+        with open(str(swag_path), 'w', encoding='UTF-8') as f:
+            json.dump(swag, f, indent=2, ensure_ascii=False)
 
+        squad_path = args.output_dir / f"{split}_squad.json"
+        with open(str(squad_path), 'w', encoding='UTF-8') as f:
+            json.dump(squad, f, indent=2, ensure_ascii=False)
 
 if __name__ == "__main__":
     main()
--- HW2/train.sh
+++ HW2/train.sh
@@ -11,18 +11,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-if [ ! -d data ]; then
-mkdir -p data
-wget https://www.dropbox.com/s/ou7a07f2af2kxrf/ntu-adl-hw2-fall-2022.zip?dl=1 -O data.zip
-unzip data.zip -d data && rm data.zip
+if [ ! -d data/origin ]; then
+    mkdir -p data/origin
+    wget https://www.dropbox.com/s/ou7a07f2af2kxrf/ntu-adl-hw2-fall-2022.zip?dl=1 -O data.zip
+    unzip data.zip -d data/origin && rm data.zip
 fi
 
-python3 preprocess.py --data_dir data \
---output_dir data/preprocessed \
+python3 preprocess.py \
+--data_dir data/origin \
+--output_dir data/preprocessed
 
-accelerate launch run_swag_no_trainer.py \
---train_file data/preprocessed/train.json \
---validation_file data/preprocessed/valid.json \
+accelerate launch src/Multiple_Choice/run_multiple_choice.py \
+--train_file data/preprocessed/train_swag.json \
+--validation_file data/preprocessed/valid_swag.json \
 --max_length 512 \
 --pad_to_max_length \
 --model_name_or_path bert-base-chinese \
@@ -31,7 +32,7 @@ accelerate launch run_swag_no_trainer.py \
 --num_train_epochs 1 \
 --gradient_accumulation_steps 2 \
 --output_dir tmp/test-swag-no-trainer \
---seed 2022 \
+--seed 2022
 
 # --dataset_name \
 # --dataset_config_name \
@@ -52,6 +53,56 @@ accelerate launch run_swag_no_trainer.py \
 # --model_type \
 # --debug \
 # --hub_model_id \
+# --hub_token \
+# --checkpointing_steps \
+# --resume_from_checkpoint \
+# --with_tracking \
+# --report_to \
+
+accelerate launch run_qa_no_trainer.py \
+--train_file data/preprocessed/train_squad.json \
+--validation_file data/preprocessed/valid_squad.json \
+--max_seq_length 512 \
+--pad_to_max_length \
+--model_name_or_path bert-base-chinese \
+--per_device_train_batch_size 1 \
+--learning_rate 3e-5 \
+--num_train_epochs 3 \
+--gradient_accumulation_steps 2 \
+--output_dir tmp/debug_squad \
+--seed 2022
+
+# --dataset_name \
+# --dataset_config_name \
+
+# --preprocessing_num_workers
+# --do_predict \
+# --test_file \
+
+# --config_name \
+# --tokenizer_name \
+# --use_slow_tokenizer \
+
+# --per_device_eval_batch_size \
+
+# --weight_decay \
+
+# --max_train_steps \
+
+# --lr_scheduler_type \
+# --num_warmup_steps \
+
+# --doc_stride \
+# --n_best_size \
+# --null_score_diff_threshold \
+# --version_2_with_negative \
+# --max_answer_length \
+# --max_train_samples \
+# --max_eval_samples \
+# --overwrite_cache \
+# --max_predict_samples \
+# --model_type \
+# --push_to_hub \
 # --hub_token \
 # --checkpointing_steps \
 # --resume_from_checkpoint \
