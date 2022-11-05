f6391168c02806b9b59de5b400c04e9bb8a1d18d
youxin1231 <tommy135721@gmail.com>
2022-11-05 14:17:40 +0800

    sh: change run_*.py to train_*.py

--- HW2/run.sh
+++ HW2/run.sh
@@ -1,4 +1,4 @@
-MODEL_NAME = "bert_base_chinese"
+MODEL_NAME="bert_base_chinese"
 
 python3 src/preprocess.py \
 --test_preprocess \
@@ -6,13 +6,13 @@ python3 src/preprocess.py \
 --test_file "${2}" \
 --output_file data/preprocessed/test_swag.json
 
-python3  src/Multiple_Choice/run_multiple_choice.py \
+python3  src/Multiple_Choice/test_multiple_choice.py \
 --model_name_or_path ckpt/"${MODEL_NAME}"/multiple-choice \
 --test_file data/preprocessed/test_swag.json
 --output_file data/multiple_choice_pred.json \
 
 
-python3  src/Multiple_Choice/run_multiple_choice.py \
+python3  src/Multiple_Choice/test_question_answering.py \
 --model_name_or_path ckpt/"${MODEL_NAME}"/question_answering \
 --test_file data/multiple_choice_pred.json
 --output_file "${3}" \
