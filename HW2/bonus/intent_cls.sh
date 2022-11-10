403f4d693996ffa71e0bbdfe788061daf59b918e
youxin1231 <tommy135721@gmail.com>
2022-11-10 23:16:54 +0800

    sh: Update sh after finish intent

--- HW2/bonus/intent_cls.sh
+++ HW2/bonus/intent_cls.sh
@@ -1,6 +1,7 @@
-python3 src/test_intent.py \
---test_file data/intent/test.json \
---ckpt_dir ckpt/intent \
---batch_size 64 \
---device cuda \
+MODEL_NAME="bert-base-uncased"
+
+accelerate launch src/test_intent.py \
+--test_file data/origin/intent/test.json \
+--ckpt_dir ckpt/"${MODEL_NAME##*/}"/intent \
+--batch_size 32 \
 --output_file intent_result.csv
--- HW2/bonus/train.sh
+++ HW2/bonus/train.sh
@@ -1,9 +1,24 @@
-python3 src/train_intent.py \
---train_file data/intent/train.json \
---eval_file data/intent/eval.json \
---batch_size 1 \
+MODEL_NAME="bert-base-uncased"
+
+if [ ! -d data/origin ]; then
+    mkdir -p data/origin
+    kaggle competitions download -c intent-classification-ntu-adl-hw1-fall-2022
+    unzip intent-classification-ntu-adl-hw1-fall-2022.zip -d data/origin/intent && rm intent-classification-ntu-adl-hw1-fall-2022.zip && rm data/origin/intent/sampleSubmission.csv
+    kaggle competitions download -c slot-tagging-ntu-adl-hw1-fall-2022
+    unzip slot-tagging-ntu-adl-hw1-fall-2022.zip -d data/origin/slot && rm slot-tagging-ntu-adl-hw1-fall-2022.zip && rm data/origin/slot/sampleSubmission-2.csv
+fi
+
+accelerate launch src/train_intent.py \
+--model_name_or_path "${MODEL_NAME}" \
+--train_file data/origin/intent/train.json \
+--validation_file data/origin/intent/eval.json \
+--max_length 50 \
+--pad_to_max_length \
+--per_device_train_batch_size 32 \
+--per_device_eval_batch_size 32 \
 --learning_rate 3e-5 \
---num_epochs 1 \
---output_dir ckpt/intent \
---seed 2022 \
---device cuda 
+--weight_decay 1e-2 \
+--num_train_epochs 8 \
+--gradient_accumulation_steps 2 \
+--output_dir ckpt/"${MODEL_NAME##*/}"/intent \
+--seed 2022
