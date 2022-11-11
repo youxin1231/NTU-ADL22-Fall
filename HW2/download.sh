733f92a2ab20a2b6bbf68230f2655a7166e93fc7
youxin1231 <tommy135721@gmail.com>
2022-11-11 16:12:09 +0800

    sh: Update download.sh

--- HW2/download.sh
+++ HW2/download.sh
@@ -4,4 +4,4 @@ unzip ckpt.zip && rm ckpt.zip
 
 # Bonus
 wget https://www.dropbox.com/s/qha3k6f40qhp4pt/bonus_ckpt.zip?dl=1 -O bonus_ckpt.zip
-unzip bonus_ckpt.zip -d bonus && rm bonus_ckpt.zip
+unzip bonus_ckpt.zip -d bonus && rm bonus_ckpt.zip && rm -r bonus/__MACOSX
