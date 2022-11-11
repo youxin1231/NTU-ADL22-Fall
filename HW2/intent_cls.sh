38cd7de0bab6c2fd9d43a23a0b23251714dfc70a
youxin1231 <tommy135721@gmail.com>
2022-11-11 16:12:31 +0800

    sh: Fix intent_cls.sh bug

--- HW2/intent_cls.sh
+++ HW2/intent_cls.sh
@@ -1,6 +1,6 @@
 MODEL_NAME="bert-base-uncased"
 
-accelerate launch bonus/src/test_intent.py \
+python3 bonus/src/test_intent.py \
 --test_file "${1}" \
 --ckpt_dir bonus/ckpt/"${MODEL_NAME##*/}"/intent \
 --batch_size 32 \
