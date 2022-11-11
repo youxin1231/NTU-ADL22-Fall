30f85f3cc6ca63fac09a02ab96ebbbe47ac251cc
youxin1231 <tommy135721@gmail.com>
2022-11-11 14:07:00 +0800

    sh: Finish download.sh

--- HW2/download.sh
+++ HW2/download.sh
@@ -1,2 +1,7 @@
+# HW2
 wget https://www.dropbox.com/s/ezh2ily6lzale1o/ckpt.zip?dl=1 -O ckpt.zip
-unzip ckpt.zip -d ckpt && rm ckpt.zip
+unzip ckpt.zip && rm ckpt.zip
+
+# Bonus
+wget https://www.dropbox.com/s/qha3k6f40qhp4pt/bonus_ckpt.zip?dl=1 -O bonus_ckpt.zip
+unzip bonus_ckpt.zip -d bonus && rm bonus_ckpt.zip
